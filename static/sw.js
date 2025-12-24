a691a5a75873e3da497569925d181e0a090199e7
rached <anesracrached88@gmail.com>
2025-12-24 10:24:22 +0100

    Update Monetag to new zone 10368997

--- static/sw.js
+++ static/sw.js
@@ -1,3 +1,3 @@
-self.options = { "domain": "3nbf4.com", "zoneId": 10368933 };
+self.options = { "domain": "5gvci.com", "zoneId": 10368997 };
 self.lary = "";
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
